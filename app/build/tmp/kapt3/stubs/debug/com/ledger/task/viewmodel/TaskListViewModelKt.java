a22cf5532744f18c94a8a1980a0a6e65f0a79ea6
ljh <766626059@qq.com>
2026-04-18 17:39:36 +0800

    添加 .gitignore，移除本地配置文件

--- app/build/tmp/kapt3/stubs/debug/com/ledger/task/viewmodel/TaskListViewModelKt.java
+++
@@ -1,8 +0,0 @@
-package com.ledger.task.viewmodel;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010$\n\u0002\u0018\u0002\n\u0002\u0010\b\n\u0000\"\u001a\u0010\u0000\u001a\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0004"}, d2 = {"DISPLAY_STATUS_ORDER", "", "Lcom/ledger/task/data/model/DisplayStatus;", "", "app_debug"})
-public final class TaskListViewModelKt {
-    private static final java.util.Map<com.ledger.task.data.model.DisplayStatus, java.lang.Integer> DISPLAY_STATUS_ORDER = null;
-}
